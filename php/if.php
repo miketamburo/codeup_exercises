f86426fb94776a43bea4ac21ee146a3d2c73beb7
miketamburo <matsatam@gmail.com>
2014-02-06 14:48:33 -0600

    Control Structures I Lesson 3 of 5
    if.php file edited to address using else and else if

--- php/if.php
+++ php/if.php
@@ -7,6 +7,9 @@ $c = '10';
 if ($a < $b) {
 	// output the appropriate result
 	echo "$a is less than $b\n";
+} else if ($a > $b){
+	// outp ut the appropriate result
+	echo "$a is greater than $b\n";
 }
 
 if ($b > $a) {
@@ -17,32 +20,26 @@ if ($b > $a) {
 if ($b >= $c) {
 	// output the appropriate result
 	echo "$b is greater than or equal to $c\n";
-}
-
-if ($b <= $c) {
+} else {
 	// output the appropriate result
 	echo "$b is less than or equal to $c\n";
 }
 
-if ($b == $c) {
-	//output the appropriate result
-	echo "$b is equal to $c\n";
-}
+// start of next section
 
 if ($b === $c) {
+	// Changed the output to show the values of the variables are identical
+	
+	echo "The value of $b is identical to the value $c\n";
+} else {
+	if ($b == $c) {
 	// output the appropriate result
-	echo "$b is identical to $c\n";
-}
-
-// output the4 appropriate result
-if ($b != $c) {
+	echo "$b is equal to $c\n";
+	} else if ($b != $c) {
 	echo "$b is not equal to $c\n";
-}
-
-// output the appropriate result
-if ($b !== $c) {
+	} else {
 	echo "$b is not identical to $c\n";
+	}
 }
-
 ?>
 
