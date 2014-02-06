9259d1b4bdccd6a94e16da704bde386553b6fd65
miketamburo <matsatam@gmail.com>
2014-02-06 15:54:10 -0600

    Part two: counting down from 100 by 5's

--- php/do-while.php
+++ php/do-while.php
@@ -4,6 +4,13 @@ $number = 0;
 do {
 	echo $number . "\n";
 	$number = $number + 2;
-} while ($number <= 100)
+} while ($number < 100);
+//now count backwards by 5's from 100 to -10
+
+do {
+	echo $number . "\n";
+	$number = $number - 5;
+} while ($number >= -10)
+
 
 ?>
