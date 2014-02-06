428531a1851d3be658b64e8136a0314de01eab19
miketamburo <matsatam@gmail.com>
2014-02-06 16:01:19 -0600

    The last part of the lesson.  Echo  when multiplying  by  less than 1,000,000

--- php/do-while.php
+++ php/do-while.php
@@ -1,16 +1,10 @@
 <?php
 
-$number = 0;
-do {
-	echo $number . "\n";
-	$number = $number + 2;
-} while ($number < 100);
-//now count backwards by 5's from 100 to -10
+$a = 2;
 
 do {
-	echo $number . "\n";
-	$number = $number - 5;
-} while ($number >= -10)
-
+	echo $a . "\n";
+	$a = $a * $a;
+} while ($a < 1000000);
 
 ?>
