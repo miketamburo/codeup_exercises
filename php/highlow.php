ada048d1ad7a2a4557690b6c2b5527a844c6307d
miketamburo <matsatam@gmail.com>
2014-02-07 12:00:33 -0600

    HighLow Version 2
    experimenting with different ways to solve the problem

--- php/highlow.php
+++ php/highlow.php
@@ -1,32 +1,39 @@
 <?php
 // The High Low Game!
 // Pick a random number between 1 and 100
-$rand_number = rand(1, 100);
+$rand_number = mt_rand(1, 100);
 
 // Ask the user to guess the number
 echo "Welcome to the High / Low game! " . " Guess a number between 1 and 100. ";
-$guess = fgets(STDIN);
+$num_Of_Guesses = 0;
 
 // Determine if the user's guess is higher or lower than the computer's number
 do { 
+	
+	echo "Guess? ";
+	$guess = fgets(STDIN);
 
-	if ($guess == $rand_number) {
+// Count the number of guesses
+	$num_Of_Guesses++;
+
+// Addresses the chance they guess the number on their first try.
+	if (($guess == $rand_number) && ($num_Of_Guesses == 1)) {
 		echo "You win!  Lucky you. \n";
+	
 	} elseif ($guess < $rand_number) {
 		echo "HIGHER! \n";
-		echo "Guess? ";
-		$guess = fgets(STDIN);
-	} else {
-		echo "LOWER! \n";
-		echo "Guess? ";
-		$guess = fgets(STDIN);
+		
+	} elseif ($guess > $rand_number) {
+		echo "LOWER! \n";	
 	}	
 
 
 } while ($guess != $rand_number);
+
 // Tell the user they won!
 // To remove the carriage return I just used the value of the random number variable vs the STDIN
 echo "WINNER!  The number was $rand_number!\n";
+echo "It took you $num_Of_Guesses tries.\n";
 exit();
 
 
