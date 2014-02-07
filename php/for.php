d1f3ecdc377835003e33cb623eb0426a27767c8b
miketamburo <matsatam@gmail.com>
2014-02-07 16:26:19 -0600

    Control Structures II
    Part 2 of lesson - declaring increment value

--- php/for.php
+++ php/for.php
@@ -7,9 +7,13 @@ $starting_number = intval(fgets(STDIN));
 fwrite(STDOUT, 'Enter the second number: ');
 $ending_number = intval(fgets(STDIN));
 
+fwrite(STDOUT, 'Enter your desired counting increment. ');
+$increment = intval(fgets(STDIN));
+
 //Then display all numbers from the staring to ending using a for loop.
-for ($i = 0; $i<$ending_number; $i++) {	
-	echo $starting_number++ . "\n";
+for ($i = 0; $i<$ending_number; $i = $i + $increment) {	
+	echo "$starting_number\n";
+	$starting_number = $starting_number + $increment;
 }
 
 ?>
