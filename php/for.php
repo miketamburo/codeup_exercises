a40396e385dab3187b93ddcbd8eba2cfbf792fcf
miketamburo <matsatam@gmail.com>
2014-02-07 17:25:12 -0600

    Control Structures II
    Give an error message if something other than integers are entered

--- php/for.php
+++ php/for.php
@@ -2,14 +2,25 @@
 
 // Prompt a user for a starting number and an ending number.
 fwrite(STDOUT, 'Please enter a starting number and an ending number. Enter the first number. ');
-$starting_number = intval(fgets(STDIN));
+$starting_number = fgets(STDIN);
 
 fwrite(STDOUT, 'Enter the second number: ');
-$ending_number = intval(fgets(STDIN));
+$ending_number = fgets(STDIN);
+
+$starting_number = trim($starting_number);
+$ending_number = trim($ending_number);
+
+
+// Test to see if entries are numeric
+ if (!is_numeric($starting_number) || !is_numeric($ending_number)) {
+ 	echo "Error!  You did not enter numbers!\n";
+ 	exit(0);
+ }
+ 
 
 fwrite(STDOUT, 'Enter your desired counting increment. ');
 $increment = intval(fgets(STDIN));
-if ($increment == NULL) {
+if ($increment === 0) {
 	$increment = 1;
 	echo "You didn't enter a value.  We will count by 1s.\n";
 }
