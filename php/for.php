d1f166f7db8c069e6ff024324d4549ff21cb95f4
miketamburo <matsatam@gmail.com>
2014-02-07 16:29:15 -0600

    Control Structures II
    Default increment to 1 if no input.

--- php/for.php
+++ php/for.php
@@ -9,6 +9,10 @@ $ending_number = intval(fgets(STDIN));
 
 fwrite(STDOUT, 'Enter your desired counting increment. ');
 $increment = intval(fgets(STDIN));
+if ($increment == NULL) {
+	$increment = 1;
+	echo "You didn't enter a value.  We will count by 1s.\n";
+}
 
 //Then display all numbers from the staring to ending using a for loop.
 for ($i = 0; $i<$ending_number; $i = $i + $increment) {	
