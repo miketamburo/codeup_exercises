dbafd630a461a6e5abecbfe9508ad49aa5461897
miketamburo <matsatam@gmail.com>
2014-02-07 18:14:35 -0600

    Highlow
    Experimenting with the high low game

--- php/highlow.php
+++ php/highlow.php
@@ -1,10 +1,28 @@
 <?php
 // The High Low Game!
-// Pick a random number between 1 and 100
-$rand_number = mt_rand(1, 100);
+// Check that the arguments total the 3 required
+if (($argc == 3)&&((is_numeric($argv[1])!=(is_numeric($argv[2])) {
+			do {	
+				// Figure out which number is the minimum and which one is the maximum number.
+				if ($argv[1] > $argv[2]) {
+					$min = $argv[2];
+					$max = $argv[1];
+				} else {
+					$min = $argv[1];
+					$max = $argv[2];
+				}
+		} while ($argv[1]  $argv[2]);  
 
-// Ask the user to guess the number
-echo "Welcome to the High / Low game! " . " Guess a number between 1 and 100. ";
+} else {
+	echo "You didn't enter the correct information.  We will play by guessing a number between 1 and 100";
+	$min = 1;
+	$max = 100;
+}
+	
+// Generate the random myster number:
+$rand_number = mt_rand($min, $max);	
+
+echo "Welcome to the High / Low game! " . " Guess a number between $min and $max. ";
 $num_Of_Guesses = 0;
 
 // Determine if the user's guess is higher or lower than the computer's number
@@ -32,9 +50,11 @@ do {
 
 // Tell the user they won!
 // To remove the carriage return I just used the value of the random number variable vs the STDIN
-echo "WINNER!  The number was $rand_number!\n";
+
+echo "GOOD GUESS!  The number was $rand_number!\n";
 echo "It took you $num_Of_Guesses tries.\n";
 exit();
+}
 
 
 ?>
