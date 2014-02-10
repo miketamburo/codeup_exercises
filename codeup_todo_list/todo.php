c74e70babfa3c29ecb2272ccedfa8de294f77fd1
miketamburo <matsatam@gmail.com>
2014-02-10 16:50:57 -0600

    Todo_list
    Renamed array items from “1” on

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -3,16 +3,26 @@
 // Create array to hold list of todo items
 $items = array();
 
+
 // The loop!
+
 do {
     // Iterate through list items
     foreach ($items as $key => $item) {
         // Display each item and a newline
+        // use a switch to exclude array key = 0
+       switch ($key) {  
+            case 0:
+            // echo "\n"; option to add a new line if desired
+            break;
+
+            default:      
         echo "[{$key}] {$item}\n";
+    }
     }
 
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo "\n" . '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
@@ -24,6 +34,9 @@ do {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
+        // reserved key 0 of array
+        $items[0] = array(0); 
+        // Add user's input to array
         $items[] = trim(fgets(STDIN));
     } elseif ($input == 'R') {
         // Remove which item?
