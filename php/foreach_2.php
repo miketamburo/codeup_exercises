b9312daaf18b05814cf71719e0b64b884eb51430
miketamburo <matsatam@gmail.com>
2014-02-10 13:12:44 -0600

    foreach_2 exercise Book Info
    Prints out books and their info

---
+++ php/foreach_2.php
@@ -0,0 +1,41 @@
+<?php
+
+// Control Structures II
+// Using foreach loops with keys and values
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+// Run the foreach loop
+
+foreach ($books as $key => $value){
+	echo "$key\n";
+	foreach ($value as $key2 => $value2) {
+		echo "\t$key2: $value2\n";
+	}
+	
+	}
+
+exit(0);
+
+
+?>
