a91673b45ff8508e7757ef8c39433a6b3fb60b3b
miketamburo <matsatam@gmail.com>
2014-02-10 14:34:11 -0600

    break_continue exercise with continue command
    uses continue command

--- php/break_continue.php
+++ php/break_continue.php
@@ -2,9 +2,10 @@
 // Create a for loop that shows all even numbers between 1 and 100
 
 for ($i = 1; $i < 101; $i++) {
-	if ($i % 2 == 0) {
-		echo $i . "\n";
+	if ($i % 2 != 0) {
+		continue;
 	}
+		echo $i . "\n";
 }
 
 
