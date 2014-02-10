49c55025ec4f2da954d2674de6980cbc86ef34de
miketamburo <matsatam@gmail.com>
2014-02-10 13:31:34 -0600

    Book List (foreach_2)
    Added comments

--- php/foreach_2.php
+++ php/foreach_2.php
@@ -28,8 +28,11 @@ $books = array(
 // Run the foreach loop
 
 foreach ($books as $key => $value){
+	// $key is the name of the imbedded array as well as the key for the parent array
+	// $value is the value assigned to each key of the parent array
 	//echo "$key\n"; commented out for part 3 of exercise
 		foreach ($value as $key2 => $value2) {
+			// $key2 is the key of the child array and $value2 is the value assigned to the key
 			if ($key2 == 'published' && $value2 >= 1950){
 			echo "$key was published in $value2\n";
 			}
