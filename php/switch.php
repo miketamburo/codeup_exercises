347ed8277a890855420e0b8d0b9f0a365898dab8
miketamburo <matsatam@gmail.com>
2014-02-10 15:32:11 -0600

    switch version 2 - highlight the weekend
    adds comment to Saturday and Sunday

--- php/switch.php
+++ php/switch.php
@@ -5,7 +5,9 @@ date_default_timezone_set('America/Chicago');
 
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
-$day_of_week = date('N');
+// $day_of_week = date('N');
+$day_of_week = rand(1, 7);
+$weekend_marker = 0;
 
 switch($day_of_week) {
     case 1:
@@ -25,13 +27,19 @@ switch($day_of_week) {
         break;
     case 6:
     	echo "Saturday\n";
+    	$weekend_marker = 1;
         break;
     case 7:
     	echo "Sunday\n";
+    	$weekend_marker = 1;
         break;
 
     default:
     	echo "default";
-}    	    
+}    
+//  echo $day_of_week;	
+if ($day_of_week >= 6) {
+	echo "Welcome the weekend!\n";
+}    
   	
 ?>
