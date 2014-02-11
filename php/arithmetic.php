3b802a6027be18f613972c46e121916b95d6df8e
miketamburo <matsatam@gmail.com>
2014-02-11 12:47:46 -0600

    Function I Lesson 2
    default values and variable handling

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -13,21 +13,32 @@ function multiply($num1, $num2) {
 }
 
 function divide($num1, $num2) {
+	if ($num2 == 0) {
+		echo "Didn't you learn in elementary school you can divide by zero?\n";
+	} else {
     echo $num1 / $num2 . PHP_EOL;
+	}
 }
 
 function modulus($num1, $num2) {
+    if ($num2 == 0) {
+		echo "Didn't you learn in elementary school you can divide by zero?\n";
+	} else {
     echo $num1 % $num2 . PHP_EOL;
+    }
 }
 
 $num1 = 40;
-$num2 = 2;
+$num2 = 0;
 
-add($num1, $num2);
-subtract($num1, $num2);
-multiply($num1, $num2);
-divide($num1, $num2);
-modulus($num1, $num2);
-
+if (is_numeric($num1) && (is_numeric($num2))) {
+	add($num1, $num2);
+	subtract($num1, $num2);
+	multiply($num1, $num2);
+	divide($num1, $num2);
+	modulus($num1, $num2);
+} else {
+	echo "The inputs are not numeric.\n";
+}
 
 ?>
