7730c6962860c9aaeaa985b596306b5050ab9afd
miketamburo <matsatam@gmail.com>
2014-02-11 15:11:03 -0600

    Lesson Functions
    changed loops

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,42 +1,58 @@
 <?php
 // The add function 
 function add($num1, $num2) {
-	numeric_check($num1, $num2);
-     return ($num1 + $num2);
+	if (numeric_check($num1, $num2) == true){
+    	return ($num1 + $num2);
+ 	} else {
+ 		exit(0);
+ 	}
 }
 
 // The subtraction function
 function subtract($num1, $num2) {
-	numeric_check($num1, $num2);
-     return ($num1 - $num2);
+	if (numeric_check($num1, $num2) == true){
+     	return ($num1 - $num2);
+ 	} else {
+ 		exit(0);
+ 	}
 }
 
 // The multiply function
 function multiply($num1, $num2) {
-	numeric_check($num1, $num2);
-    return ($num1 * $num2);
+	if (numeric_check($num1, $num2) == true){
+     	return ($num1 * $num2);
+ 	} else {
+ 		exit(0);
+ 	}
 }
 
 // The divide function
 function divide($num1, $num2) {
-	numeric_check($num1, $num2);
-
+	if (numeric_check($num1, $num2) == true){
+     	return ($num1 + $num2);
+ 	} else {
+ 		exit(0);
+ 	}
 	if ($num2 == 0) {
 		return false;
 	} else {	
-    	 return ($num1 / $num2);
+    	return ($num1 / $num2);
 	}
 }
 
 // The modulus function
 function modulus($num1, $num2) {
-	numeric_check($num1, $num2);
+	if (numeric_check($num1, $num2) == true){
+     	return ($num1 + $num2);
+ 	} else {
+ 		exit(0);
+ 	}
     if ($num2 == 0) {
     	return false;
 		// echo "Didn't you learn in elementary school you can't divide by zero?\n";
 		// echo "The input values were:  $num1 and $num2.\n";
 	} else {	
-    	 return ($num1 % $num2);
+    	return ($num1 % $num2);
 	}
 }
 
@@ -46,12 +62,12 @@ function numeric_check ($num1, $num2) {
 		return true;
 	} else {
 		return false;
-		exit(0);
+		
 	}
 } 
 
 $num1 = 40;
-$num2 = 10;
+$num2 = 0;
 
 	add($num1, $num2);
 	subtract($num1, $num2);
