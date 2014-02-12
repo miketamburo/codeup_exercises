5dbcd8fa53037ed7c6c3700f7c3f5f70addae065
miketamburo <matsatam@gmail.com>
2014-02-12 17:32:02 -0600

    update to code

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -5,32 +5,41 @@ $names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-$query = 'cat';
 // Create a function to compare 2 arrays that returns the number of values in common.
-function compare ($array1, $array2) {
-	
-}
+
+
 
 // Create a function that returns TRUE or FALSE
 function find_it ($query, $names) {
-	foreach ($names as $name) {
 
-	return (array_search($query, $names)) ? TRUE : FALSE;
+	return (array_search($query, $names) !== FALSE) ? TRUE : FALSE;
+
 		// if (array_search($query, $names) !== FALSE) {
 		// 	echo $name . PHP_EOL;
 		// 	return $name;
-
 		// 	break;
 		// } else {
 		// 	echo "The name wasn't found." . PHP_EOL;
 		// 	return FALSE;
 		// }
+	
+}
+
+function compare ($names, $compare) {
+	foreach ($names as $key => $value) {
+		find_it($value, $compare);
+		var_dump(find_it($value, $compare));
 	}
 }
+	
+
+
 
 find_it('Tina', $names);
 find_it('Bob', $names);
-	
 
+compare ($names, $compare);
+	
+?>
 
 
