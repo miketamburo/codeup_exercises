f416d4ed881a6b55b2bc8163785528b166110eb8
miketamburo <matsatam@gmail.com>
2014-02-12 13:20:55 -0600

    ToDo List with functions
    Simplified code with functions

---
+++ codeup_todo_list/todo_update_w_functions.php
@@ -0,0 +1,66 @@
+<?php
+
+// Create array to hold list of todo items
+$items = array();
+
+// List array items formatted for CLI
+function list_items($list) {
+    // Return string of list items separated by newlines.
+    // Should be listed [KEY] Value like this:
+    // [1] TODO item 1
+    // [2] TODO item 2 - blah
+    $string = '';
+    foreach ($list as $key => $value) {
+        $key = $key + 1;
+        // Display each item and a newline
+        $string .= "[" . $key . "]" . " " . $value . " item " . $key . PHP_EOL;
+    // DO NOT USE ECHO, USE RETURN
+    }
+        return $string;
+}
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = FALSE) {
+
+    $input = trim(fgets(STDIN));
+
+    return $upper ? strtoupper($input) : $input;
+
+}
+
+// The loop!
+do {
+    // Echo the list produced by the function
+    echo list_items($items);
+
+    // Show the menu options
+    echo '(N)ew item, (R)emove item, (Q)uit : ' . PHP_EOL;
+
+    // Get the input from user
+    // Use trim() to remove whitespace and newlines
+    $input = get_input(TRUE);
+
+    // Check for actionable input
+    if ($input == 'N') {
+        // Ask for entry
+        echo 'Enter item: ';
+        // Add entry to list array
+        $items[] = get_input();
+    } elseif ($input == 'R') {
+        // Remove which item?
+        echo 'Enter item number to remove: ';
+        // Get array key
+        $key = get_input(TRUE) - 1;
+        // Remove from array
+        unset($items[$key]);
+        $items = array_values($items);
+    }
+// Exit when input is (Q)uit
+} while ($input != 'Q');
+
+// Say Goodbye!
+echo "Goodbye!" .PHP_EOL;
+
+// Exit with 0 errors
+exit(0);
