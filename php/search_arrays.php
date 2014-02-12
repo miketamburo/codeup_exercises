9cf58a1e4875feb43f94b09f8e292b283ace3285
miketamburo <matsatam@gmail.com>
2014-02-12 17:45:53 -0600

    Array Search
    Completed Exercise

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -7,33 +7,27 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 // Create a function to compare 2 arrays that returns the number of values in common.
 
-
-
 // Create a function that returns TRUE or FALSE
 function find_it ($query, $names) {
 
 	return (array_search($query, $names) !== FALSE) ? TRUE : FALSE;
-
-		// if (array_search($query, $names) !== FALSE) {
-		// 	echo $name . PHP_EOL;
-		// 	return $name;
-		// 	break;
-		// } else {
-		// 	echo "The name wasn't found." . PHP_EOL;
-		// 	return FALSE;
-		// }
-	
 }
 
 function compare ($names, $compare) {
+		$names_in_common = 0;
 	foreach ($names as $key => $value) {
+		
 		find_it($value, $compare);
-		var_dump(find_it($value, $compare));
+
+		if (find_it($value, $compare) !== FALSE) {
+			$names_in_common = $names_in_common + 1;
+		}
+		// test it -- remove after code review
+		// var_dump(find_it($value, $compare));
 	}
+			echo "Matching names = $names_in_common" . PHP_EOL;
 }
 	
-
-
 
 find_it('Tina', $names);
 find_it('Bob', $names);
