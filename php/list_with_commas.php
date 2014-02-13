d47dbcc22aba64aa1a8cef0bac5ab3dcae8582ee
miketamburo <matsatam@gmail.com>
2014-02-13 16:23:01 -0600

    List with command shortened version

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -2,32 +2,26 @@
 
 //Use the $physicists_array
 $physicists_array = array('Gordon Freeman', 'Samantha Carter', 'Sheldon Cooper', 'Quinn Mallory', 'Bruce Banner', 'Tony Stark');
-function computerized_list($string) {
-	$new_array_name = explode(',', $string);
-	return $new_array_name;
-}
-
-// Converts array into list n1, n2, ..., and n3
-function humanized_list($array) {
-	// Find the last name of the array.
-	$end_name = array_pop($array);
-	
-	$new_string_name = (implode(',', $array)) . ", and{$end_name}";
-	return $new_string_name;
-}
-
 // List of famous peeps
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
+
+function humanized_list($input, $sort = false) {
+	$explode = explode(',', $input);
+	$end_name = array_pop($explode);
+	
+		if ($sort == true){
+		sort ($explode);
+		}
+	$new_string_name = (implode(',', $explode)) . ", and{$end_name}";
+	return $new_string_name;
+}
+
+
 // Humanize that list
-$famous_fake_physicists = humanized_list(computerized_list($physicists_string));
+$famous_fake_physicists = humanized_list($physicists_string, TRUE);
+ 
+echo "Some of the most famous fictional theoretical physicists are{$famous_fake_physicists}." . PHP_EOL;
 
-
-
-// Output sentence
-echo "Some of the most famous fictional theoretical physicists are $famous_fake_physicists.";
 
 ?>
-
-
-
