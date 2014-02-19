328c312f6ef1aa7964dff3a502d41650514c7a21
miketamburo <matsatam@gmail.com>
2014-02-18 19:35:46 -0600

    morse_code_2 - morse code to English (Completed Project)

--- php/morse_code_2.php
+++ php/morse_code_2.php
@@ -3,207 +3,186 @@
 // Morse Code 2.0
 // Turn Morse Code into English
 
+function look_up($generated_letter){
+
+				switch($generated_letter){
+					case '-':
+						echo "T";
+						break;
+					case '--':
+						echo "M";
+						break;
+					case '---':
+						echo "O";
+						break;
+					case '--.':
+						echo "G";
+						break;
+					case '--.-':
+						echo "Q";
+						break;
+					case '--..':
+						echo "Z";
+						break;
+					case '-.':
+						echo "N";
+						break;
+					case '-.-':
+						echo "K";
+						break;
+					case '-.--':
+						echo "Y";
+						break;
+					case '-.-.':
+						echo "C";
+						break;
+					case '-..':
+						echo "D";
+						break;
+					case '-..-':
+						echo "X";
+						break;
+					case '-...':
+						echo "B";
+						break;
+					case '-':
+						echo "E";
+						break;
+					case '.-':
+						echo "A";
+						break;
+					case '.--':
+						echo "W";
+						break;
+					case '.---':
+						echo "J";
+						break;
+					case '.--.':
+						echo "P";
+						break;
+					case '.-.':
+						echo "R";
+						break;
+					case '.-..':
+						echo "L";
+						break;
+					case '..':
+						echo "I";
+						break;
+					case '..-':
+						echo "U";
+						break;
+					case '..-.':
+						echo "F";
+						break;
+					case '...':
+						echo "S";
+						break;
+					case '...-':
+						echo "V";
+						break;
+					case '....':
+						echo "H";
+						break;
+					case '-----':
+						echo "0";
+						break;
+					case '----.':
+						echo "9";
+						break;
+					case '---..':
+						echo "8";
+						break;
+					case '--...':
+						echo "7";
+						break;
+					case '-....':
+						echo "6";
+						break;
+					case '.----':
+						echo "1";
+						break;
+					case '..---':
+						echo "2";
+						break;
+					case '...--':
+						echo "3";
+						break;
+					case '....-':
+						echo "4";
+						break;
+					case '.....':
+						echo "5";
+						break;
+					case '---...':
+						echo ":";
+						break;
+					case '--..--':
+						echo ",";
+						break;
+					case '-.--.-':
+						echo ")";
+						break;
+					case '-.--.-':
+						echo "(";
+						break;
+					case '-.-.--':
+						echo "!";
+						break;
+					case '-.-.-.':
+						echo ";";
+						break;
+					case '-..-.':
+						echo "/";
+						break;
+					case '-...-':
+						echo "=";
+						break;
+					case '-....-':
+						echo "-";
+						break;
+					case '.-.-.':
+						echo "+";
+						break;
+					case '.-.-.-':
+						echo ".";
+						break;
+					case '.-..-.':
+						echo '"';
+						break;
+					case '.-...':
+						echo "&";
+						break;
+					case '..--.-':
+						echo "_";
+						break;
+					case '..--..':
+						echo "?";
+						break;
+					case '...-..-':
+						echo "$";
+						break;
+				}	
+}
+
 // Get the Morse Code
 echo "Please enter your Morse Code.  Use '/' instead of spaces between words." . PHP_EOL;
 $input_code = (strtoupper(trim(fgets(STDIN))));
 
-// echo (substr($input_code, 0, 1)) . PHP_EOL;
-// 	echo (substr($input_code, 1, 1)) . PHP_EOL;
-// 		echo (substr($input_code, 2, 1)) . PHP_EOL;
-// 			echo (substr($input_code, 3, 1)) . PHP_EOL;
-// 				var_dump((substr($input_code, 3, 1))) . PHP_EOL;
-// 					echo (substr($input_code, 5, 1)) . PHP_EOL;
-$code_length = strlen($input_code);
+$input_array = explode("/", $input_code);
 
-$generated_letter = 0;
-$letter = 0;
+foreach($input_array as $key => $value){
+	$converted_value_array = explode(" ", $value);
 
-
-for ($i=0; $i<=$code_length; $i++){
-		if ($letter !== " "){
-			$letter = (substr($input_code, $i, 1));
-			$generated_letter .= $letter;
-
-		} elseif ($letter === "/") {
-			echo " ";
-		} elseif ($letter === " "){
-			if ($i <= $code_length){
-
-			} else {
-			$input_code = substr($input_code, $i+1);	
-			}
+		foreach ($converted_value_array as $converted_letter) {
+			look_up($converted_letter);
 		}
+	echo " ";
+		
 }
-
-$generated_letter = (trim($generated_letter));
-$generated_letter = (substr($generated_letter, 1));
-
-	switch($generated_letter){
-		case '-':
-			echo "T";
-			break;
-		case '--':
-			echo "M";
-			break;
-		case '---':
-			echo "O";
-			break;
-		case '--.':
-			echo "G";
-			break;
-		case '--.-':
-			echo "Q";
-			break;
-		case '--..':
-			echo "Z";
-			break;
-		case '-.':
-			echo "N";
-			break;
-		case '-.-':
-			echo "K";
-			break;
-		case '-.--':
-			echo "Y";
-			break;
-		case '-.-.':
-			echo "C";
-			break;
-		case '-..':
-			echo "D";
-			break;
-		case '-..-':
-			echo "X";
-			break;
-		case '-...':
-			echo "B";
-			break;
-		case '-':
-			echo "E";
-			break;
-		case '.-':
-			echo "A";
-			break;
-		case '.--':
-			echo "W";
-			break;
-		case '.---':
-			echo "J";
-			break;
-		case '.--.':
-			echo "P";
-			break;
-		case '.-.':
-			echo "R";
-			break;
-		case '.-..':
-			echo "L";
-			break;
-		case '..':
-			echo "I";
-			break;
-		case '..-':
-			echo "U";
-			break;
-		case '..-.':
-			echo "F";
-			break;
-		case '...':
-			echo "S";
-			break;
-		case '...-':
-			echo "V";
-			break;
-		case '....':
-			echo "H";
-			break;
-
-		case '-----':
-			echo "0";
-			break;
-		case '----.':
-			echo "9";
-			break;
-		case '---..':
-			echo "8";
-			break;
-		case '--...':
-			echo "7";
-			break;
-		case '-....':
-			echo "6";
-			break;
-		case '.----':
-			echo "1";
-			break;
-		case '..---':
-			echo "2";
-			break;
-		case '...--':
-			echo "3";
-			break;
-		case '....-':
-			echo "4";
-			break;
-		case '.....':
-			echo "5";
-			break;
-
-		case '---...':
-			echo ":";
-			break;
-		case '--..--':
-			echo ",";
-			break;
-		case '-.--.-':
-			echo ")";
-			break;
-		case '-.--.-':
-			echo "(";
-			break;
-		case '-.-.--':
-			echo "!";
-			break;
-		case '-.-.-.':
-			echo ";";
-			break;
-		case '-..-.':
-			echo "/";
-			break;
-		case '-...-':
-			echo "=";
-			break;
-		case '-....-':
-			echo "-";
-			break;
-		case '.-.-.':
-			echo "+";
-			break;
-		case '.-.-.-':
-			echo ".";
-			break;
-		case '.-..-.':
-			echo '"';
-			break;
-		case '.-...':
-			echo "&";
-			break;
-		case '..--.-':
-			echo "_";
-			break;
-		case '..--..':
-			echo "?";
-			break;
-		case '...-..-':
-			echo "$";
-			break;
-	}
-		if ($i == $code_length){
-			break;
-		} else {
-		$input_code = (substr($input_code, $i + 1));
-		}
+				
 
 echo "\n";
+exit(0);
 
 ?>
