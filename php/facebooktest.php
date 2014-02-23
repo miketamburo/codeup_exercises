aacf299920571a6dad73e017a78f77a84e6e412a
miketamburo <matsatam@gmail.com>
2014-02-23 16:30:06 -0600

    Facebook Test Completed

--- php/facebooktest.php
+++ php/facebooktest.php
@@ -1,26 +1,21 @@
 <?php
 // FACEBOOK test
 // Take a set of integers, calculate the space between each and find the missing number
-$input = "1 3 5 9 11";
+$input = "1 6 16 21 26";
 $diff_array = [];
 $input_array = explode(" ", $input);
-// $sum = ceil((array_sum($input_array) / 6) / 5) * 2;
+
 for ($i=1; $i < (count($input_array)); $i++){
 	$diff = $input_array[$i]-$input_array[$i-1];
 	$diff_array[] = $diff;
 }
-	var_dump($diff_array);
-$diff_array = array_unique($diff_array);
-$diff_count_array = array_count_values($diff_array);
-$key = array_search(1, $diff_count_array);
-var_dump($diff_array);
-var_dump($diff_count_array);
-echo $key;
-// the missing number is the number following the key with the double int value.
-// to calculate the missing number, it is the key value plus half the int value.
-foreach ($diff_array as $value) {
-	if ($value != 2){
-		echo "The missing number is " . ($input_array[$key] + $value/2) . PHP_EOL;
+
+$diffValue = $diff_array[0];
+foreach ($diff_array as $key => $value) {
+	if ($value != $diffValue){
+		echo "The interval is: " . $diffValue . " and the missing number is " . (($input_array[$key]) + $diffValue) . PHP_EOL;
 	}
 }
+
+
 ?>
