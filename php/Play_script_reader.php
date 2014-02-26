a8e1ca3a652aa0581f832f0d3b51dfe81d9a52b2
miketamburo <matsatam@gmail.com>
2014-02-25 22:12:38 -0600

    Play reader - work in progress

--- php/Play_script_reader.php
+++ php/Play_script_reader.php
@@ -10,44 +10,53 @@ to help him edit his scripts. You're program should take in the name
 of his character, open the script text, and output only the lines for 
 his character.
 */
-
-
+$matches = array();
 // Welcome the straving actor
 echo "Welcome to the script Script" . PHP_EOL;
 
 // Get the name of the play that they are acting in
+do {
 echo "Please enter the number of the script you wish to use." . PHP_EOL;
 echo "(1) The Importance of Being Earnest\n(2) Much Ado About Nothing\n(3) Pygmalion";
 echo "\n";
 $scriptChoice = (trim(fgets(STDIN)));
-if ($scriptChoice == 1) {
-	$script = 'challenges_data/earnest.txt';
-} elseif ($scriptChoice == 2) {
-	$script = 'challenges_data/muchado.txt';
-} elseif ($scriptChoice == 3) {
-	$script = 'challenges_data/pygmallion.txt';
-}
 
+	if ($scriptChoice == '1') {
+		$filename = 'challenges_data/earnest.txt';
+		break;
+	} elseif ($scriptChoice == '2') {
+		$filename = 'challenges_data/muchado.txt';
+		break;
+	} elseif ($scriptChoice == '3') {
+		$filename = 'challenges_data/pygmallion.txt';
+		break;
+	} 
+
+} while ($scriptChoice != '1' || $scriptChoice != '2' || $scriptChoice != '3');
 
 // Get their character name
-echo "Please enter the name of your character as it is entered in the script for character cue.  For example:  Algernon Moncrieff is Algernon." . PHP_EOL;
+echo "Please enter the name of your character as it is entered in the script for character cue.\nFor example:  Algernon Moncrieff is Algernon." . PHP_EOL;
 
 $character_name = ucfirst(strtolower(trim(fgets(STDIN))));
-$matches = array();
 
 // Open the play's text file
-$filename = $script;
 $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
 
-$contents_array = explode(' ', $contents);
+$contents_array = explode("\n", $contents);
 
 // var_dump($contents);
 // var_dump($character_name);
-
-$result = preg_match("/($character_name) (.+)/", $contents, $matches);
-var_dump($matches);
+$results = preg_split("/($character_name)./", $contents, null, PREG_SPLIT_NO_EMPTY);
+// $result = preg_match("/^($character_name) (.+)/", $contents, $matches);
+print_r($results);
+// foreach($results as $key => $result){
+// 	echo gettype($result);
+// 	if (is_array($result)){
+// 		echo $result . PHP_EOL;
+// 	}
+// }
 
 
 // Convert the play to a searchable format (as needed)
