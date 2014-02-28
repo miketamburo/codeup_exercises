9ba1d277197f238d83a86f09d2505504a5d13684
miketamburo <matsatam@gmail.com>
2014-02-28 11:38:13 -0600

    Class Filestore.php
    Created a class file for use in reading, adding and saving files.

---
+++ php/filestore.php
@@ -0,0 +1,42 @@
+<?php
+
+class Filestore {
+	public $filename = '';
+	public $items = '':
+	public $contents = [];
+
+	public function __construct($filename = ''){
+		$this->filename = $filename;
+	}
+
+	// returns array of lines in $this->filename
+	public function read_lines(){
+		$handle = fopen($this->filename, "r");
+		$contents = fread($handle, filesize($this->filename));
+		fclose($handle);
+		if (is_string($contents)){
+			$contents = explode("\n", $contents);	
+		} 	
+		$items = array_merge($this->items, $contents);
+		return array_values($this->items);
+	}
+	// writes each element in $array to a new line in $this-> filename
+	public function write_lines(){
+		$handle = fopen($this->filename, 'w');
+			foreach ($this->contents as $fields) {
+				fputcsv($handle, $fields);
+			}
+		fclose($handle);
+	}
+
+	// Reads contents of csv $this->filename, returns an array
+	public function read_csv(){
+		$contents = [];
+		$handle = fopen($this->filename, 'r');
+		while(($data = fgetcsv($handle)) !== FALSE) {
+	  	$contents[] = $data;
+		}
+		fclose($handle);
+		return $contents;
+	}
+}
