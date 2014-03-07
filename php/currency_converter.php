1234c9acc4df57b4fbc7ae78311a08d57d6af716
miketamburo <matsatam@gmail.com>
2014-03-07 16:38:40 -0600

    --message

--- php/currency_converter.php
+++ php/currency_converter.php
@@ -1,6 +1,9 @@
 <?php
 // Currency Converter (2 points)
 
+$US_dollar_euros = .73 //euros
+$dollar_lbs = .60
+$dollar_pesos = 13
 $total = 0;
 
 // User will enter the amount of money in US dollars and choose the currency they want to convert it to
@@ -10,13 +13,13 @@ $amount = (trim(fgets(STDIN)));
 echo "What currency do you want to convert to? (E)uros, (P)esos, (B)ritish Pounds." . PHP_EOL;
 $currency = (strtoupper(trim(fgets(STDIN))));
 
-if ($currency == 'E') {
+if ($currency = 'E') {
 	$total = $amount * .73;
 	$currency_name = 'euros';
-} elseif ($currency == 'P') {
+} elseif ($currency = 'P') {
 	$total = $amount * 13;
 	$currency_name = 'pesos';
-} elseif ($currency == 'B') {
+} elseif ($currency = 'B') {
 	$total = $amount * .60;
 	$currency_name = 'pounds';
 }
