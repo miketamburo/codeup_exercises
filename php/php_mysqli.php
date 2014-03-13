5322e3c1d65bec2d220bbbac40edaf3a7709b8a4
miketamburo <matsatam@gmail.com>
2014-03-13 15:39:22 -0500

    mysqli exercise using national park data

--- php/php_mysqli.php
+++ php/php_mysqli.php
@@ -9,5 +9,22 @@ if ($mysqli->connect_errno) {
 	echo $mysqli->host_info . "\n";
 }
 
+// Create the query and assign to var
+$query = 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(75) NOT NULL,
+    description VARCHAR(600) NOT NULL,
+    date_established DATE NOT NULL,
+    area_in_acres FLOAT(10, 2) NOT NULL,
+    PRIMARY KEY (id)
+);';
+
+// Run query, if there are errors then display them
+if (!$mysqli->query($query)) {
+    throw new Exception("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);
+}
+
+
 
 ?>
